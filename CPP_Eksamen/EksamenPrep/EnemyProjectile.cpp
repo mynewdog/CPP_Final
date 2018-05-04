78d8e699ab40827ea09e5667a475c6268c7feed6
loyte-markus <32316026+loyte-markus@users.noreply.github.com>
2018-05-04 17:58:04 +0200

    EnemyProjectile skall

---
+++ CPP_Eksamen/EksamenPrep/EnemyProjectile.cpp
@@ -0,0 +1,54 @@
+#include "stdafx.h"
+#include "EnemyProjectile.h"
+#include <iostream>
+#include <SDL.h>
+
+void EnemyProjectile::spawn(SDL_Renderer* render, int spawnX, int spawnY) {
+	enemyprojectile_BMP = SDL_LoadBMP("Img/playergun.bmp");
+	enemyprojectile_drawable = SDL_CreateTextureFromSurface(render, enemyprojectile_BMP);
+	enemyprojectile_coords.h = enemyprojectile_BMP->h;
+	enemyprojectile_coords.w = enemyprojectile_BMP->w;
+	enemyprojectile_coords.x = spawnX;
+	enemyprojectile_coords.y = spawnY;
+	SDL_FreeSurface(enemyprojectile_BMP);
+	move();
+}
+
+void EnemyProjectile::move() {
+	if (enemyprojectile_coords.y <= -810) {
+		enemyprojectile_coords.y += 10;
+	}
+}
+
+SDL_Rect* EnemyProjectile::getCoords() {
+	return &enemyprojectile_coords;
+}
+
+SDL_Texture* EnemyProjectile::getDrawable() {
+	return enemyprojectile_drawable;
+}
+
+int EnemyProjectile::getX() {
+	return getCoords()->x;
+}
+
+int EnemyProjectile::getY() {
+	return getCoords()->y;
+}
+
+void EnemyProjectile::setX(int x) {
+	enemyprojectile_coords.x = x;
+}
+
+void EnemyProjectile::setY(int y) {
+	enemyprojectile_coords.y = y;
+}
+
+EnemyProjectile::EnemyProjectile()
+{
+}
+
+
+EnemyProjectile::~EnemyProjectile()
+{
+}
---
+++ CPP_Eksamen/EksamenPrep/EnemyProjectile.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <SDL.h>
+
+class EnemyProjectile
+{
+public:
+	EnemyProjectile();
+	~EnemyProjectile();
+	void move();
+	void spawn(SDL_Renderer* render, int spawnX, int spawnY);
+
+	SDL_Texture* getDrawable();
+	SDL_Rect* getCoords();
+
+	void setX(int x);
+	void setY(int y);
+	int getX();
+	int getY();
+
+private:
+	int m_projectileX;
+	int m_projectileY;
+
+	SDL_Surface * enemyprojectile_BMP;
+	SDL_Texture * enemyprojectile_drawable;
+	SDL_Rect enemyprojectile_coords;
+};
+
